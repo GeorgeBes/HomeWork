977152a62cf96b79014f29f84ed73eaba939985c
George <bortnikow1991@mail.ru>
2022-10-08 12:31:19 +0300

    Решил 19 задачу

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -8,3 +8,20 @@
 // 12821 -> да
 
 // 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit > 10000 && digit < 100000)
+{
+    int firstDigit = digit / 10000;
+    int secondDigit = digit / 1000 % 10;
+    int fourthDigit = digit / 10 % 10;
+    int fifthDigit = digit % 10;
+    if (firstDigit == fifthDigit && secondDigit == fourthDigit)
+    {
+        Console.WriteLine("Да");
+    }
+    else Console.WriteLine("Нет");
+}
+else Console.WriteLine("Число не пятизначное!!!");

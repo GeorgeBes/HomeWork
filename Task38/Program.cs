ef17893ac7700eaf46720d1d533e6eef852a2c7f
George <bortnikow1991@mail.ru>
2022-10-19 14:45:00 +0300

    Решил 38 задачу

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -6,15 +6,15 @@
 
 
 
-double[] CreateArrayRndDouble(int size, int min, int max) // Метод для создания массива
+double[] CreateArrayRndDouble(int size, int min, int max) // Метод для создания массива double
 {
     var array = new double[size];
     var rnd = new Random();
 
     for (int i = 0; i < size; i++)
     {
-        double rndNum = rnd.NextDouble()*(max-min)+min;
-        array[i] = Math.Round(rndNum,2);
+        double rndNum = rnd.NextDouble() * (max - min) + min;
+        array[i] = Math.Round(rndNum, 2);
     }
     return array;
 }
@@ -24,11 +24,41 @@ void PrintArray(double[] array) // Метод печати массива
     Console.Write("[");
     for (int i = 0; i < array.Length; i++)
     {
-        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        if (i < array.Length - 1) Console.Write($"{array[i]} ");
         else Console.Write($"{array[i]}");
     }
-    Console.WriteLine("]");
+    Console.Write("] ");
 }
 
-double[] arr = CreateArrayRndDouble(10,-10,10);
+double SearchMax(double[] arrMax) // Поиск максимального
+{
+    double max = arrMax[0];
+    for (int i = 1; i < arrMax.Length; i++)
+    {
+        if (arrMax[i] > max) max = arrMax[i];
+    }
+    return max;
+}
+
+double SearchMin(double[] arrMin)
+{
+    double min = arrMin[0];
+    for (int i = 1; i < arrMin.Length; i++)
+    {
+        if (arrMin[i] < min) min = arrMin[i];
+    }
+    return min;
+}
+
+
+double[] arr = CreateArrayRndDouble(5, 0, 100);
 PrintArray(arr);
+
+// Console.WriteLine();
+double max = SearchMax(arr);
+// Console.WriteLine($"Max {Math.Round(max, 2)}");
+
+double min = SearchMin(arr);
+// Console.WriteLine($"Min {Math.Round(min, 2)}");
+
+Console.Write($"-> {Math.Round(max - min, 2)}");

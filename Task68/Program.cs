71f04083b9a8c5d2961bda3a6289f276398dbe35
George <bortnikow1991@mail.ru>
2022-11-19 17:05:34 +0300

    Решил 68 задачу

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -7,17 +7,17 @@ int numberM = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите целое число n");
 int numberN = Convert.ToInt32(Console.ReadLine());
 
-int AkkermanFunction(int numM,int numN)
+int AkkermanFunction(int numM, int numN)
 {
-if (numM==0) return numN+1;
-else if (numM>0&&numN==0) return AkkermanFunction(numM-1,1);
-else return AkkermanFunction(numM-1,AkkermanFunction(numM,numN-1));
+    if (numM == 0) return numN + 1;
+    else if (numM > 0 && numN == 0) return AkkermanFunction(numM - 1, 1);
+    else return AkkermanFunction(numM - 1, AkkermanFunction(numM, numN - 1));
 }
 
 int res = 0;
-if (numberM<0 || numberN<0) Console.WriteLine("Числа отрицательны");
+if (numberM < 0 || numberN < 0) Console.WriteLine("Числа отрицательны");
 else
 {
-    res = AkkermanFunction(numberM,numberN);
+    res = AkkermanFunction(numberM, numberN);
     Console.WriteLine($"A({numberM},{numberN}) = {res}");
 }
